c6d371e6c8860efb27668c5329c6d7261047abe6
Thomas Peterson <thpeter@kth.se>
2019-08-14 12:31:42 +0200

    Add trace, reg and mem info to getInput

--- SE/getInput.py
+++ SE/getInput.py
@@ -8,18 +8,25 @@ def main():
     run(program,address)
 
 def run(program, address):
-    m = Manticore(program)
+    #m = Manticore(program, pure_symbolic=True)
+    m = Manticore(program, pure_symbolic=False)
     with m.locked_context() as context:
         context['flag'] = ""
+
+    m.add_hook(None, print_ip)
 
     @m.hook(address)
     def hook(state):
         print("Reached address " + hex(address))
+        print(state.cpu)
+        print(state.mem)
         m.generate_testcase(state,address)
         m.kill()
 
     m.run()
 
+def print_ip(state):
+    print(hex(state.cpu.RIP))
 
 if __name__ == "__main__":
     main()
