8828b42a8caed713cea1a48ad47148389d31280f
Thomas Peterson <thpeter@kth.se>
2019-08-22 13:56:51 +0200

    Updated clean script

--- input/clean.sh
+++ input/clean.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-find . -type f  ! -name "*.*"  -delete
+find . -type f -name "*_jak.asm"  -delete
 find . -type f -name "*.o" -delete
+find . -type f -name "*.dot" -delete
+find . -type f -name "*.i64" -delete
