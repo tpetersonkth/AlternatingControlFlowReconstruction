773427adffe37ef184a01452a54babb6de589a36
Thomas Peterson <thpeter@kth.se>
2019-09-09 14:03:27 +0200

    update clean.sh to remove files without extensions

--- input/clean.sh
+++ input/clean.sh
@@ -3,3 +3,4 @@ find . -type f -name "*_jak.asm"  -delete
 find . -type f -name "*.o" -delete
 find . -type f -name "*.dot" -delete
 find . -type f -name "*.i64" -delete
+find . -type f  ! -name "*.*"  -delete
