166e899b6b2e9d75cf27bbec95e37005ea0d6f86
Thomas Peterson <thpeter@kth.se>
2019-09-11 09:42:28 +0200

    Update compile script to also compile c++

--- input/compile.sh
+++ input/compile.sh
@@ -5,6 +5,17 @@ file="$1"
 if [[ $file == *"."* ]]; then
         file=${file::-4}
 fi
-echo "Compiling $file.asm"
-nasm -O0 -f elf "$file.asm"
-ld -m elf_i386 -e _start -o "$file" "$file.o"
+
+fileasm="${file}.asm"
+if test -f "$fileasm"; then
+        echo "Compiling $fileasm"
+        nasm -O0 -f elf "$fileasm"
+        ld -m elf_i386 -e _start -o "$file" "$file.o"
+fi
+
+filecpp="${file}.cpp"
+if test -f "$filecpp"; then
+        echo "Compiling $filecpp"
+        g++ "$filecpp" -o "$file"
+fi
+
