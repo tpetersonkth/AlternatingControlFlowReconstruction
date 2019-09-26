b8366acb20d23ba81b776f3694dede36b140ea7c
Thomas Peterson <thpeter@kth.se>
2019-09-26 17:16:50 +0200

    Update 00cjmp.asm to be a better examlpe of constant propagation

--- input/Synthetic/Small/00cjmp.asm
+++ input/Synthetic/Small/00cjmp.asm
@@ -1,12 +1,17 @@
 ;cpa c: Can propagate eax=0 to the cmp instruction and resolves all branches without claiming that unreachable can be reached
 ;Note: Strange extra instruction in cfa graph?
-
+;Example: Good example of constant propagation basics
 SECTION .text
 
 global _start
 
 _start:
         mov eax, 0
+        add eax, 20
+        mov edi, 12
+        sub eax, 30
+        add edi, 10
+        add eax, 10
         cmp eax, 0
         je exit
 
