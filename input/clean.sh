27b101ce6d699020fbafe1271232c8c6403ccb4b
Thomas Peterson <thpeter@kth.se>
2019-09-28 18:56:46 +0200

    Modify cleaning script to also remove ida files

--- input/clean.sh
+++ input/clean.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
 find . -type f -name "*_jak.asm"  -delete
 find . -type f -name "*.o" -delete
 find . -type f -name "*.dot" -delete
-find . -type f -name "*.i64" -delete
 find . -type f  ! -name "*.*"  -delete
+
+#Delete ida files
+find . -type f -name "*.i64" -delete
+find . -type f -name "*.nam" -delete
+find . -type f -name "*.til" -delete
+find . -type f -name "*.id*" -delete
