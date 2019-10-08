9d591379182b35bb2ce25b20daf363899c6f300a
Thomas Peterson <thpeter@kth.se>
2019-10-08 09:01:01 +0200

    Add interval + interval with DSE + constant propagation with DSE to genResults.sh

--- utilities/genResults.sh
+++ utilities/genResults.sh
@@ -1,35 +1,73 @@
 #!/bin/bash
 
-#Note: Assumes that the ideal graphs are located in the results directory
+# Note: Currently assumes that the ideal graphs are located in the results directory
 
-echo "$0"
-if [ $# -ne 3 ]
+# Analyzes a binary using jakstab and a DSE engine
+# Params:
+#   path to a binary
+#   path to directory containing ideal ccfa:s
+#   directory to store results
+#   port number of DSE(TODO)
+analyzeBinary () {
+    # Rename parameters for readability
+    fullpath=$1
+    idealDir=$2
+    resultDir=$3
+    DSEPort=$4
+
+    # Extract variables
+    path=$(dirname "$fullPath")
+    file=$(basename "$fullPath")
+    fileNoExt="${file%.*}"
+    pathToBin=$(realpath ${path})/$fileNoExt
+
+    # Constant propagation
+    jak -m "$pathToBin" --cpa c
+    mv "$pathToBin"'_ccfa.dot' $(realpath "$resultDir/$fileNoExt"'_c_ccfa.dot')
+    mv "$pathToBin"'_states.dat' $(realpath "$resultDir/$fileNoExt"'_c_states.dat')
+    mv "$pathToBin"'_stats.dat' $(realpath "$resultDir/$fileNoExt"'_c_stats.dat')
+    python3 calculateStats.py $(realpath "$idealDir/$fileNoExt"'_ideal.dot') $(realpath "$resultDir/$fileNoExt"'_c_ccfa.dot')
+
+    # Interval analysis
+    jak -m "$pathToBin" --cpa i
+    mv "$pathToBin"'_ccfa.dot' $(realpath "$resultDir/$fileNoExt"'_i_ccfa.dot')
+    mv "$pathToBin"'_states.dat' $(realpath "$resultDir/$fileNoExt"'_i_states.dat')
+    mv "$pathToBin"'_stats.dat' $(realpath "$resultDir/$fileNoExt"'_i_stats.dat')
+    python3 calculateStats.py $(realpath "$idealDir/$fileNoExt"'_ideal.dot') $(realpath "$resultDir/$fileNoExt"'_i_ccfa.dot')
+
+    # Constant propagation with DSE
+    jak -m "$pathToBin" --cpa c --dse "$DSEPort" 
+    mv "$pathToBin"'_ccfa.dot' $(realpath "$resultDir/$fileNoExt"'_cD_ccfa.dot')
+    mv "$pathToBin"'_states.dat' $(realpath "$resultDir/$fileNoExt"'_cD_states.dat')
+    mv "$pathToBin"'_stats.dat' $(realpath "$resultDir/$fileNoExt"'_cD_stats.dat')
+    python3 calculateStats.py $(realpath "$idealDir/$fileNoExt"'_ideal.dot') $(realpath "$resultDir/$fileNoExt"'_cD_ccfa.dot')
+
+    # Interval analysis with DSE
+    jak -m "$pathToBin" --cpa i --dse "$DSEPort"
+    mv "$pathToBin"'_ccfa.dot' $(realpath "$resultDir/$fileNoExt"'_iD_ccfa.dot')
+    mv "$pathToBin"'_states.dat' $(realpath "$resultDir/$fileNoExt"'_iD_states.dat')
+    mv "$pathToBin"'_stats.dat' $(realpath "$resultDir/$fileNoExt"'_iD_stats.dat')
+    python3 calculateStats.py $(realpath "$idealDir/$fileNoExt"'_ideal.dot') $(realpath "$resultDir/$fileNoExt"'_iD_ccfa.dot')
+
+}
+
+if [ $# -ne 4 ]
 then
-    echo "Usage: ./genresults.sh [asm dir] [results dir] [port for DSE]"
+    echo "Usage: ./genresults.sh [asm dir] [ideal graph dir] [results dir] [port for DSE]"
 else
-    for fullPath in $1/*.asm; do
-        if [[ ${fullPath: -8} != "_jak.asm" ]]
+    for fullPath in $1*.asm; do
+        if [[ ${fullPath: -8} != "_jak.asm" ]] #Skip jakstab dissasembled assembly
         then
-            path=$(dirname "$fullPath")
-            file=$(basename "$fullPath")
-            fileNoExt="${file%.*}"
-            pathToBin=$(realpath ${path})/$fileNoExt
 
-            ../Input/compileStatic.sh $path
-
-            jak -m "$pathToBin" --cpa c
-
-            mv "$pathToBin"'_ccfa.dot' $(realpath "$2/$fileNoExt"'_c_ccfa.dot')
-            mv "$pathToBin"'_states.dat' $(realpath "$2/$fileNoExt"'_c_states.dat')
-            mv "$pathToBin"'_stats.dat' $(realpath "$2/$fileNoExt"'_c_stats.dat')
-            python3 calculateStats.py $(realpath "$2/$fileNoExt"'_ideal.dot') $(realpath "$2/$fileNoExt"'_c_ccfa.dot')
-
-            # jak -m "$path" --cpa i
-            # mv "$path"'_i_ccfa.dot' $2
-            # mv "$path"'_i_states.dot' $2
-            # mv "$path"'_i_stats.dat' $2
-            # python3 calculateStats.py "$2/$path"'_ideal.dot' "$2/$path"'_i_ccfa.dot'
+            echo "Attempting to compile $fullPath"
+            ../Input/compileStatic.sh $fullPath
+            analyzeBinary $fullPath $2 $3 $4
         fi
     done
+    for fullPath in $1*.c; do
+        fullPath="${fullPath%.*}"
+        echo "Attempting to compile $fullPath"
+        ../Input/compileMinimal.sh $fullPath #Compiles statically without stdlib
+        analyzeBinary $fullPath $2 $3 $4
+    done
 fi
-
