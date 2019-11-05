446388c0b746bb76244691ab0acf8c31da3d5e52
Thomas Peterson <thpeter@kth.se>
2019-11-05 18:56:00 +0100

    Fixed problem with set intersection not working propertly + documentation, for calculateStats.py

--- utilities/calculateStats.py
+++ utilities/calculateStats.py
@@ -8,26 +8,31 @@ import sys, os, networkx, datetime
 from presentStats import addStats
 
 def main(idealGraphFile, generatedGraphFile, statsfile):
+    #Load graphs
     tprint("Loading ideal graph")
     IGraph = load(idealGraphFile)
     tprint("Loading generated graph")
     GGraph = load(generatedGraphFile)
     tprint("Graphs loaded")
 
+    #Load stats
     stats = {}
     addStats(stats, statsfile)
 
+    #Get basename
     basename = os.path.basename(statsfile)
     basename = basename.split("_")
     analysisType = basename[1]
     basename = basename[0]
 
+    #Get text section size from stats object
     textSectionSize = int(stats[basename][analysisType]['Section .text size'],16)
 
+    #Calculate graph stats
     tprint("Starting calculations of graph stats")
     accuracy, soundness, precision, TFAccuracy, TFSoundness, TFPrecision = calculateStats(IGraph,GGraph,textSectionSize)
 
-    #Build output
+    #Print stats to stdout
     out = "\n"
     out+="Accuracy: "+percentage(accuracy)+"\n"
     out+="Soundness: "+percentage(soundness)+"\n"
@@ -35,11 +40,9 @@ def main(idealGraphFile, generatedGraphFile, statsfile):
     out+="Top Free Accuracy: "+percentage(TFAccuracy)+"\n"
     out+="Top Free Soundness: "+percentage(TFSoundness)+"\n"
     out+="Top Free Precision: "+percentage(TFPrecision)+"\n"
-
-    #Print stats to stdout
     print(out)
 
-    #Write states to a file
+    #Write stats to a file
     filename = generatedGraphFile.split(".")
     filename[-2] += "_graph_stats.dat"
     filename = ".".join(filename[:-1])
@@ -48,17 +51,39 @@ def main(idealGraphFile, generatedGraphFile, statsfile):
     fid.write(out)
     fid.close()
 
-
+'''
+Loads a graph from a file
+Params:
+    filename - A path to a .dot file
+Returns:
+    A networkx graph object corresponding to the graph in the file specified by filename
+'''
 def load(filename):
     graph = networkx.drawing.nx_pydot.read_dot(filename)
     return graph
 
+'''
+Checks if a node is a top node
+Params: 
+    graph - A graph which contains the node
+    node - A node in the graph
+Returns:
+    True if the node is a top node, False otherwise
+'''
 def isTopNode(graph, node):
     # Jakstab colors nodes red if they are tops
     if 'fillcolor' in graph._node[node] and graph._node[node]['fillcolor'] == '"red"':
         return True
     return False
 
+'''
+Converts a top graph into a graph without edges from top locations
+Params:
+    graph - The graph to convert into a top-free form
+    providedTopNodes(Opt) - A list of nodes which will be treated like top locations
+Returns: 
+    A graph that contains all edges of the graph graph except does who originate from a top location
+'''
 def getTopFreeGraph(graph, providedTopNodes=[]):
     topFreeGraph = networkx.MultiDiGraph()  # Top free has the same nodes as the ideal graph but without top edges
     topNodes = []
@@ -81,8 +106,15 @@ def getTopFreeGraph(graph, providedTopNodes=[]):
 
     return topFreeGraph, topNodes, topEdges
 
-# Calculate accuracy, soundness and precision with respect to the ideal graph
-def calculateStats(idealGraph, graph, SizeOfTextSection):
+'''
+Calculate accuracy, soundness and precision with respect to an ideal graph
+Params:
+    idealGraph - The graph which is the ground truth
+    graph - The graph to evaluate with respect to the ground truth
+    sizeOfTextSection - The size of the text section (Required for calulating the number of edges from a top node)
+Returns: The accuracy, soundness, precision, TFAccuracy, TFSoundness, TFPrecision of the graph graph with respect to the graph idealGraph
+'''
+def calculateStats(idealGraph, graph, sizeOfTextSection):
 
     tprint("Generating top free graph")
     topFreeGraph, topNodes, _ = getTopFreeGraph(graph)
@@ -90,11 +122,11 @@ def calculateStats(idealGraph, graph, SizeOfTextSection):
     topFreeIdealGraph, _, idealTopEdges = getTopFreeGraph(idealGraph, providedTopNodes=topNodes)
     tprint("Done generating top free graphs")
 
-    # Convert to sets to perform set operations in the calculations
-    TFGE = set(topFreeGraph.edges)
-    TFIGE = set(topFreeIdealGraph.edges)
+    # Convert to sets where nodes are integers to perform set operations in the calculations
+    TFGE = set([(int(i[0],16),int(i[1],16)) for i in topFreeGraph.edges])
+    TFIGE = set([(int(i[0],16),int(i[1],16)) for i in topFreeIdealGraph.edges])
     idealTopEdges = set(idealTopEdges)
-    numberOfTopEdgesOfGraph = SizeOfTextSection*len(topNodes)
+    numberOfTopEdgesOfGraph = sizeOfTextSection*len(topNodes)
 
     tprint("ideal graph top edges: ")
     for e in idealTopEdges:
@@ -120,11 +152,16 @@ def calculateStats(idealGraph, graph, SizeOfTextSection):
     return (accuracy, soundness, precision, TFAccuracy, TFSoundness, TFPrecision)
 
 '''
-Calculate accuracy, soundness, precision and precision error without compensating for tops
+Calculate accuracy, soundness and precision without compensating for tops
+Params:
+    idealGraph - The graph which is the ground truth
+    graph - The graph to evaluate with respect to the ground truth
+Returns: The accuracy, soundness, precision of the graph graph with respect to the graph idealGraph
 '''
 def calculateStatsRaw(idealGraph, graph):
-    GE = set(graph.edges)
-    IGE = set(idealGraph.edges)
+    #Convert graph edges into to a set of tuples of integers corresponding to the two nodes of the edge
+    GE = set([(int(i[0],16),int(i[1],16)) for i in graph.edges])
+    IGE = set([(int(i[0],16),int(i[1],16)) for i in idealGraph.edges])
 
     #Calculate accuracy and soundnes
     intersecting = float(len(GE.intersection(IGE)))
@@ -141,14 +178,26 @@ def calculateStatsRaw(idealGraph, graph):
 
     return (accuracy, soundness, precision)
 
+'''
+Converts a real from decimal form to its corresponding percentage
+Params:
+    decimalForm - The number in decimal form
+Returns:
+    The corresponding percentage
+'''
 def percentage(decimalForm):
     if (decimalForm == None):
         return "-"
 
     return str(100*decimalForm)+"%"
 
-def tprint(x):
-    print(str(datetime.datetime.now())+": "+str(x))
+'''
+Print a message to stdout with a timestamp
+Params:
+    message - The message to print
+'''
+def tprint(message):
+    print(str(datetime.datetime.now())+": "+str(message))
 
 if __name__ == "__main__":
     if (len(sys.argv) < 4):
