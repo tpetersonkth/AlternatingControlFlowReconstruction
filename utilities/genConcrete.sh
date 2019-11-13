8a53210ac2d0511f47680911ff3fa35c9939097d
Thomas Peterson <thpeter@kth.se>
2019-11-13 11:49:02 +0100

    Removed thesis relevant files and directories from master

--- utilities/genConcrete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for filename in $1/*.asm; do
-        echo "filename: $filename"
-        ../input/compileStatic.sh $filename
-
-        filename=${filename::-4}
-        jak -m "$filename" --cpa i
-        mv "$filename"'_ccfa.dot' $2
-done
